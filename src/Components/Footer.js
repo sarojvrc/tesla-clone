25b2060bb0d0a5019cde94ac203f47e83abbf663
sarojvrc <saroj.vrc@gmail.com>
2021-11-25 15:28:57 +0530

    Footer added

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Footer.css";
+
+function Footer() {
+    return (
+        <footer>
+            © Tesla Clone made by Saroj
+        </footer>
+    )
+}
+
+export default Footer
